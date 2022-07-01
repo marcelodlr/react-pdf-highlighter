d3f8b6b0212d8b980f94fc9f971a85ae357ec26b
marcelodlr <marcelodelima.r@gmail.com>
2022-07-01 11:41:47 -0300

    Update get-client-rects.ts
    Changed algorithm to extract rects

--- src/lib/get-client-rects.ts
+++ src/lib/get-client-rects.ts
@@ -2,24 +2,6 @@ import type { LTWHP, Page } from "../types.js";
 
 import optimizeClientRects from "./optimize-client-rects";
 
-const isClientRectInsidePageRect = (clientRect: DOMRect, pageRect: DOMRect) => {
-  if (clientRect.top < pageRect.top) {
-    return false;
-  }
-  if (clientRect.bottom > pageRect.bottom) {
-    return false;
-  }
-  if (clientRect.right > pageRect.right) {
-    return false;
-  }
-  if (clientRect.left < pageRect.left) {
-    return false;
-  }
-
-  return true;
-};
-
-
 const measureText = (
   el: HTMLElement,
   startOffset: number = 0,
@@ -29,18 +11,19 @@ const measureText = (
   const canvas = document.createElement("canvas");
   const ctx = canvas.getContext("2d", { alpha: false });
   if (ctx) {
-    console.log(el.textContent?.substring(startOffset, endOffset));
     ctx.font = `${el.style.fontSize} ${el.style.fontFamily}`;
     const { width } = ctx.measureText(
       el.textContent?.substring(startOffset, endOffset) || ""
     );
     return width;
   }
-  document.removeChild(canvas);
   return 0;
 };
 
-const getAllSpansInRange = (range: Range): HTMLElement[] => {
+const getAllSpansInRange = (
+  range: Range,
+  pages: Page[]
+): { node: HTMLElement; pageNumber: number }[] => {
   const firstNode = (
     range.startContainer.nodeName === "SPAN"
       ? range.startContainer
@@ -53,21 +36,24 @@ const getAllSpansInRange = (range: Range): HTMLElement[] => {
       : range.endContainer.parentNode
   ) as HTMLElement;
 
-  const spans = [firstNode];
+  const spans = [{ node: firstNode, pageNumber: pages[0].number }];
   let currentNode = firstNode;
-
+  let pageIndex = 0;
+  let currentPage = pages[0];
   if (firstNode === lastNode) return spans;
 
   do {
     if (!currentNode.nextElementSibling) {
       // The PDF.js structure is span < div.TextLayer < div.page
-      const nextPage =
-        currentNode.parentNode?.parentNode?.nextSibling?.lastChild;
-      currentNode = nextPage?.firstChild as HTMLElement;
+      pageIndex++;
+      currentPage = pages[pageIndex];
+      const nextPageTextLayer = currentPage.node.querySelector(".textLayer");
+      currentNode = nextPageTextLayer?.firstChild as HTMLElement;
     } else {
       currentNode = currentNode.nextElementSibling as HTMLElement;
     }
-    if (currentNode.nodeName === "SPAN") spans.push(currentNode);
+    if (currentNode.nodeName === "SPAN")
+      spans.push({ node: currentNode, pageNumber: currentPage.number });
   } while (currentNode !== lastNode);
 
   return spans;
@@ -91,7 +77,7 @@ interface Options {
  * 3) the last span which the selection might not include all of it's contents, we adjust width of it.
  * */
 const calcWidthAndLeftBounds = (
-  spans: HTMLElement[],
+  spans: { node: HTMLElement; pageNumber: number }[],
   span: HTMLElement,
   range: Range,
   { initialValues, scale }: Options
@@ -100,17 +86,18 @@ const calcWidthAndLeftBounds = (
     left: initialValues.left,
     width: initialValues.width,
   };
-  if (spans.indexOf(span) === 0 && spans.indexOf(span) === spans.length - 1) {
+  const spanIndex = spans.findIndex((s) => s.node == span);
+  if (spanIndex === 0 && spanIndex === spans.length - 1) {
     const selectedTextWidth =
       measureText(span, range.startOffset, range.endOffset) * scale;
     const endTextWidth = measureText(span, range.endOffset) * scale;
     bounds.left = bounds.left + bounds.width - selectedTextWidth - endTextWidth;
     bounds.width = selectedTextWidth;
-  } else if (spans.indexOf(span) === 0) {
+  } else if (spanIndex === 0) {
     const selectedTextWidth = measureText(span, range.startOffset) * scale;
     bounds.left = bounds.left + bounds.width - selectedTextWidth;
     bounds.width = selectedTextWidth;
-  } else if (spans.indexOf(span) === spans.length - 1) {
+  } else if (spanIndex === spans.length - 1) {
     bounds.width = measureText(span, 0, range.endOffset) * scale;
   }
 
@@ -123,31 +110,29 @@ const getClientRects = (
   shouldOptimize: boolean = true
 ): Array<LTWHP> => {
   const rects: LTWHP[] = [];
-  const spans = getAllSpansInRange(range);
+  const spans = getAllSpansInRange(range, pages);
 
-  for (const span of spans) {
-    for (const page of pages) {
-      const styles = getComputedStyle(span);
+  for (const { node: span, pageNumber } of spans) {
+    const styles = getComputedStyle(span);
 
-      // PDF.js applies a scaleX that it's no automatically aplied to styles.width, we need to adjust that.
-      const spanScaleX = new WebKitCSSMatrix(styles.transform).a;
-      let initialWidth = spanScaleX * fromPxToNumber(styles.width);
-      let initialLeft = fromPxToNumber(styles.left);
+    // PDF.js applies a scaleX that isn't automatically aplied to styles.width, we need to adjust that.
+    const spanScaleX = new WebKitCSSMatrix(styles.transform).a;
+    let initialWidth = spanScaleX * fromPxToNumber(styles.width);
+    let initialLeft = fromPxToNumber(styles.left);
 
-      const { width, left } = calcWidthAndLeftBounds(spans, span, range, {
-        initialValues: { width: initialWidth, left: initialLeft },
-        scale: spanScaleX,
-      });
+    const { width, left } = calcWidthAndLeftBounds(spans, span, range, {
+      initialValues: { width: initialWidth, left: initialLeft },
+      scale: spanScaleX,
+    });
 
-      const highlightedRect = {
-        top: fromPxToNumber(styles.top),
-        left: left,
-        width: width,
-        height: fromPxToNumber(styles.height),
-        pageNumber: page.number,
-      } as LTWHP;
-      rects.push(highlightedRect);
-    }
+    const highlightedRect = {
+      top: fromPxToNumber(styles.top),
+      left: left,
+      width: width,
+      height: fromPxToNumber(styles.height),
+      pageNumber: pageNumber,
+    } as LTWHP;
+    rects.push(highlightedRect);
   }
 
   return shouldOptimize ? optimizeClientRects(rects) : rects;
